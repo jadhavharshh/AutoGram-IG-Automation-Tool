e18e06ee4628bb1550828f92be54723627a4fdfe
jadhavharshh <harshjadhavconnect@gmail.com>
2025-01-07 16:18:13 +0530

    Add API client setup and Dashboard component

---
+++ server/models/UserModel.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+import { genSalt , hash } from 'bcrypt';
+
+const UserSchema = new mongoose.Schema({
+    email : {
+        type : String, 
+        required : true,
+        unique : true
+    },
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    firstName : {
+        type : String,
+        required : false,
+    },
+    lastName:{
+        type : String,
+        required : false,
+    },
+    profileSetup:{
+        type:Boolean,
+        required:false,
+        default:false,
+    }
+});
+
+
+UserSchema.pre("save", async function (next) {
+    const salt = await genSalt();
+    this.password = await hash(this.password, salt);
+})
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
