ee53920308ef6304d01d17f39b1a02c715118e59
jadhavharshh <harshjadhavconnect@gmail.com>
2025-01-09 00:05:50 +0530

    Implement toggle between Sign Up and Login in LoginForm component

--- client/src/components/ui/login-form.tsx
+++ client/src/components/ui/login-form.tsx
@@ -2,17 +2,22 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useState } from "react"
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"form">) {
+
+  const [isSignUp, setIsSignUp] = useState(true)
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">{isSignUp ? "Create your account" : "Login to your account"}</h1>
         <p className="text-balance text-sm text-muted-foreground">
-          Enter your email below to login to your account
+        {isSignUp
+            ? "Enter your details below to create an account."
+            : "Enter your email below to login to your account."}
         </p>
       </div>
       <div className="grid gap-6">
@@ -23,17 +28,27 @@ export function LoginForm({
         <div className="grid gap-2">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
+            {isSignUp && (
+                <a
+                href="#"
+                className="ml-auto text-sm underline-offset-4 hover:underline"
+              >
+                Forgot your password?
+              </a>
+            )}
           </div>
+          
           <Input id="password" type="password" required />
         </div>
+        {!isSignUp && (
+          <div className="gap-2 flex flex-col items-start">
+            <Label htmlFor="confirmPassword">Confirm Password</Label>
+            <Input id="confirmPassword" type="password" required />
+          </div>
+        )}
+        
         <Button type="submit" className="w-full">
-          Login
+          {isSignUp ? "Sign Up" : "Login"}
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-background px-2 text-muted-foreground">
@@ -47,14 +62,33 @@ export function LoginForm({
               fill="currentColor"
             />
           </svg>
-          Login with Google
+          {isSignUp ? "Sign Up with Google" : "Login with Google"}
         </Button>
       </div>
       <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <a href="#" className="underline underline-offset-4">
-          Sign up
-        </a>
+      {isSignUp ? (
+            <>
+              Already have an account?{" "}
+              <a
+                href="#"
+                className="underline underline-offset-4"
+                onClick={() => setIsSignUp(false)}
+              >
+                Login
+              </a>
+            </>
+          ) : (
+            <>
+              Don't have an account?{" "}
+              <a
+                href="#"
+                className="underline underline-offset-4"
+                onClick={() => setIsSignUp(true)}
+              >
+                Sign Up
+              </a>
+            </>
+          )}
       </div>
     </form>
   )
