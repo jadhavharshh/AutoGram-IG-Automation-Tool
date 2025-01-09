2ce870f70427a0430681f67de8a15f1082c4a8c5
jadhavharshh <harshjadhavconnect@gmail.com>
2025-01-10 00:35:51 +0530

    Add UI components: Skeleton, Collapsible, Separator, Tooltip, Avatar; implement mobile hook and navigation components

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,21 +1,56 @@
-import userAppStore from "@/store/store";
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
 
 const Dashboard = () => {
-  const { userInfo } = userAppStore();
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-center text-2xl font-bold">Dashboard</h1>
-      {userInfo ? (
-        <div className="mt-4 text-center">
-          {/* <p><strong>Name:</strong> {userInfo.name}</p> */}
-          <p><strong>Email:</strong> {userInfo.email}</p>
-          <p><strong>ID:</strong> {userInfo.id}</p>
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-slate-100/50 dark:bg-slate-800/50" />
+            <div className="aspect-video rounded-xl bg-slate-100/50 dark:bg-slate-800/50" />
+            <div className="aspect-video rounded-xl bg-slate-100/50 dark:bg-slate-800/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-slate-100/50 md:min-h-min dark:bg-slate-800/50" />
         </div>
-      ) : (
-        <p className="mt-4 text-center">No user information available.</p>
-      )}
+      </SidebarInset>
+    </SidebarProvider>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
