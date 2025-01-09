55b61310977a4d65055accb04987826b5f58a0cf
jadhavharshh <harshjadhavconnect@gmail.com>
2025-01-09 15:36:12 +0530

    Add sign-up functionality and integrate Sonner for notifications

--- client/src/components/ui/login-form.tsx
+++ client/src/components/ui/login-form.tsx
@@ -3,6 +3,9 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react"
+import { toast } from "sonner"
+import { apiClient } from "@/lib/api-client"
+import { SIGNUP_API } from "@/utils/constants"
 
 export function LoginForm({
   className,
@@ -10,8 +13,61 @@ export function LoginForm({
 }: React.ComponentPropsWithoutRef<"form">) {
 
   const [isSignUp, setIsSignUp] = useState(true)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const handleSignUp = async () => {
+    const isValid = await validateSignUp();
+    if(isValid){
+      try {
+        const response = await apiClient.post(SIGNUP_API, {email , password , confirmPassword})
+        console.log('response', response)
+        if(response.status === 201){
+          toast.success('Account created successfully')
+        }
+        if(response.status === 400){
+          toast.error(response.data.message)
+        }
+      } catch (error) {
+        console.error(error)
+    }
+    }
+  }
+
+  const validatePassword = (password : string) => {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]:;"'<>,.?/-]).{8,}$/;
+  
+    if (!passwordRegex.test(password)) {
+      return false;
+    }
+    return true;
+  }
+
+  
+  const validateSignUp = async () => {
+    if (email === "" || password === "" || confirmPassword === "") {
+      toast.error('All fields are required');
+      return false;
+    }
+    
+    if (!validatePassword(password)) {
+      toast.error('Password must include at least one uppercase letter, one lowercase letter, one number, and one special character, and be at least 8 characters long');
+      return false;
+    }
+  
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+      return false;
+    }
+  
+    return true;
+  }
+  
+
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <form className={cn("flex flex-col gap-4", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">{isSignUp ? "Create your account" : "Login to your account"}</h1>
         <p className="text-balance text-sm text-muted-foreground">
@@ -23,7 +79,7 @@ export function LoginForm({
       <div className="grid gap-6">
         <div className="flex flex-col gap-2 items-start">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input id="email" type="email" placeholder="m@example.com" value={email} required onChange={(e)=> setEmail(e.target.value)}/>
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
@@ -38,16 +94,16 @@ export function LoginForm({
             )}
           </div>
           
-          <Input id="password" type="password" required />
+          <Input id="password" type="password" required value={password} onChange={(e)=> setPassword(e.target.value)}/>
         </div>
         {isSignUp && (
           <div className="gap-2 flex flex-col items-start">
             <Label htmlFor="confirmPassword">Confirm Password</Label>
-            <Input id="confirmPassword" type="password" required />
+            <Input id="confirmPassword" type="password" value={confirmPassword} required onChange={(e)=> setConfirmPassword(e.target.value)}/>
           </div>
         )}
         
-        <Button type="submit" className="w-full">
+        <Button type="submit" className="w-full" onClick={handleSignUp}>
           {isSignUp ? "Sign Up" : "Login"}
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
@@ -55,7 +111,7 @@ export function LoginForm({
             Or continue with
           </span>
         </div>
-        <Button variant="outline" className="w-full">
+        <Button variant="outline" className="w-full" >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path
               d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/ui/theme-provider.tsx'
-
+import { Toaster } from './components/ui/sonner.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> */}
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <App />
-    {/* </ThemeProvider> */}
+    <Toaster closeButton/>
+    </ThemeProvider>
   </StrictMode>,
 )
