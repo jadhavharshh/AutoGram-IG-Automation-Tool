b5fae48cb8a2ecd1e6b61341a9c2ac129e779dbd
Harsh Jadhav <harshjadhavconnect@gmail.com>
2025-01-27 09:06:28 +0000

    Enhance UI components with sidebar and breadcrumb navigation across multiple pages

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -1,8 +1,45 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { ModeToggle } from "@/components/ui/mode-toggle"
+import { Separator } from "@/components/ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
 
 const Settings = () => {
-  return (
-    <div>Settings</div>
-  )
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-screen">
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                        <div className="flex items-center gap-2 px-4 w-full">
+                            <SidebarTrigger className="-ml-1" />
+                            <Separator orientation="vertical" className="mr-2 h-4" />
+                            {/* Container for Breadcrumb and ModeToggle */}
+                            <div className="flex justify-between items-center w-full">
+                                <Breadcrumb>
+                                    <BreadcrumbList>
+                                        <BreadcrumbPage className="">Configure Settings</BreadcrumbPage>
+                                    </BreadcrumbList>
+                                </Breadcrumb>
+                                <ModeToggle />
+                            </div>
+                        </div>
+                    </header>
+                </SidebarInset>
+            </SidebarProvider>
+        </div>
+    )
 }
 
 export default Settings
