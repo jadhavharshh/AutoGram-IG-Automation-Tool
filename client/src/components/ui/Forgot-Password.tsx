28fe29920364b2733f12eeff20c63b386cc4a467
jadhavharshh <harshjadhavconnect@gmail.com>
2025-02-08 20:21:02 +0530

    Fix Issue

--- client/src/components/ui/Forgot-Password.tsx
+++ client/src/components/ui/Forgot-Password.tsx
@@ -11,7 +11,6 @@ import {
   InputOTPGroup,
   InputOTPSlot,
 } from "@/components/ui/input-otp"
-import { useNavigate } from "react-router-dom"
 
 export function ForgotPasswordForm({ onBack }: { onBack: () => void }) {
   const [email, setEmail] = useState("");
