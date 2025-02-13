dfba09e20dc2427ec77d4f135764ab6d23481d76
jadhavharshh <harshjadhavconnect@gmail.com>
2025-02-13 15:10:18 +0530

    Remove commented-out StrictMode in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,10 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/ui/theme-provider.tsx'
 import { Toaster } from './components/ui/sonner.tsx'
+
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <App />
     <Toaster closeButton/>
     </ThemeProvider>
-  //</StrictMode>
 )
